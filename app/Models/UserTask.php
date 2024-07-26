75b6d2c552b024e574630f5512d8bcd97aaf1a9c
jajarija434 <canoed.636.stud@cdd.edu.ph>
2024-07-27 04:33:13 +0800

    UserTaskController

---
+++ app/Models/UserTask.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserTask extends Model
+{
+    use Hasfactory;
+    protected $table = 'user_task';
+    protected $guarded = [];
+}
