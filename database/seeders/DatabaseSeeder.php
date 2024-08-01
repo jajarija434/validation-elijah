0884b5f012a4f3ba9f1ca64df4aa035672c80ece
jajarija434 <canoed.636.stud@cdd.edu.ph>
2024-08-01 11:33:32 +0800

    paginator

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
 use App\Models\User;
+use App\Models\UserTask;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory(10)->create([
+           // 'name' => 'Test User',
+            //'email' => 'test@example.com',
         ]);
     }
 }
